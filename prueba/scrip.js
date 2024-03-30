6153dcea0e5bbe12cb7aec13a30a792bd5ec7e23
Carlos Mil Jimenez <c7432192@gmail.com>
2024-03-30 17:52:26 -0600

    agregue el css y script con su carpeta

--- prueba/scrip.js
+++ prueba/scrip.js
@@ -1,3 +1,14 @@
-console.log('Hola mundo')
-let nombre = 'Carlos'
-console.log('nombre')
+document.addEventListener('DOMContentLoaded', function() {
+    var guessButton = document.getElementById('guessButton');
+  
+    guessButton.addEventListener('click', function() {
+      var guessInput = document.getElementById('guessInput');
+      var guess = parseInt(guessInput.value);
+  
+      if (guess === 8) {
+        document.getElementById('message').innerHTML = '¡Felicidades! ¡Has encontrado el número!';
+      } else {
+        document.getElementById('message').innerHTML = 'Intenta de nuevo. El número era 8.';
+      }
+    });
+  });  
